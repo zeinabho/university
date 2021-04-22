2f7b1e11c560598201cbccf952d3300e2679ac4b
Z&F <h.121212.mohandes@gmail.com>
2021-04-22 16:25:30 +0430

    init commit

---
+++ src/com/github/Main.java
@@ -0,0 +1,13 @@
+package com.github;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("*");
+	int[] array={1,2,3};
+	int[] arr={0,2,9};
+	for(int m:array){
+        System.out.println(m);
+    }
+    }
+}
